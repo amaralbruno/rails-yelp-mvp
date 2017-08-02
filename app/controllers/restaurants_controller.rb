bd5ab73a5f158082ae3c49849457aaba1f0f80bf
Bruno Amaral <bru.amaral88@gmail.com>
2017-08-02 15:19:55 -0300

    all done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,6 @@
 class RestaurantsController < ApplicationController
-   before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+
+  before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
 
   def index
     @restaurants = Restaurant.all
@@ -9,9 +10,32 @@ class RestaurantsController < ApplicationController
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def edit
+  end
+
+  def update
+    if @restaurant.update(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @restaurant.destroy
+    redirect_to restaurants_path
   end
 
   private
@@ -21,6 +45,6 @@ class RestaurantsController < ApplicationController
   end
 
   def restaurant_params
-    params.require(:restaurant).permit(:name, :address, :phone_number)
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
